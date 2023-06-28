b280dada1cddc4f3906fde851e16e792365c5561
Sarthak <sarthaksri017@gmail.com>
2023-06-29 02:42:54 +0530

    ss

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItems/>
+      <div className="container my-3">
+        <h2>Top headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItems title="myTitle" description="hehe" />
+          </div>
+          <div className="col-md-4">
+            <NewsItems />
+          </div>
+          <div className="col-md-4">
+            <NewsItems />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
